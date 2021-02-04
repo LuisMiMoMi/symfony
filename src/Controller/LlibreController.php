a91f67e84d33bde8b5612862aefe5b746316c3e8
LuisMiMoMi <luismontellmira@gmail.com>
2021-02-04 17:51:30 +0100

    Commit Twig LlibresLuis

--- src/Controller/LlibreController.php
+++ src/Controller/LlibreController.php
@@ -2,12 +2,13 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-class LlibreController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class LlibreController extends AbstractController
 {
     private $llibres = array(
-    array("isbn"  =>  "A111B3",  "titol"  =>  "El   joc   d'Ender",  "autor"=>"Orson Scott Card", "pàgines" => 350),
-    array("isbn"  =>  "A222B3",  "titol"  =>  "Shrek",  "autor"=>"No ho se", "pàgines" => 1000),
-    array("isbn"  =>  "A333B3",  "titol"  =>  "Harry Potter",  "autor"=>"Patatuela", "pàgines" => 23345632)
+    array("isbn"  =>  "A111B3",  "titol"  =>  "El   joc   d'Ender",  "autor"=>"Orson Scott Card", "pagines" => 350),
+    array("isbn"  =>  "A222B3",  "titol"  =>  "Shrek",  "autor"=>"No ho se", "pagines" => 1000),
+    array("isbn"  =>  "A333B3",  "titol"  =>  "Harry Potter",  "autor"=>"Patatuela", "pagines" => 23345632)
     );
     /**
     * @Route("/llibre/{isbn}", name="fitxa_llibre")
@@ -20,16 +21,10 @@ class LlibreController
             return $llibre["isbn"] == $isbn;
         });
         if (count($resultat) > 0) {
-            $resposta = "";
-            $resultat = array_shift($resultat);
-            $resposta .= "<ul><li>Titol: " . $resultat["titol"] . "</li>" .
-            "<li>Autor/a: " . $resultat["autor"] . "</li>" .
-            "<li>Pàgines: " . $resultat["pàgines"] . "</li></ul>";
-            return new Response("<html><body>$resposta</body></html>");
+            return $this->render('fitxa_llibre.html.twig', array('llibre' => array_shift($resultat)));
         } else {
-            return new Response("Llibre no trobat");
+            return $this->render('fitxa_llibre.html.twig', array('llibre' => null));
         }
-        return new Response("Dades del llibre amb isbn $isbn");
     }
 }
 ?>
